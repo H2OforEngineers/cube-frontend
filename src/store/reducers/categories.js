e01efc8d9ebe1b81c53f3eb0c41c40b92a77bef8
siltan-elayan <sultan.m.elayan@gmail.com>
2021-09-30 18:07:07 +0300

    fixing shcema

--- src/store/reducers/categories.js
+++ src/store/reducers/categories.js
@@ -1,20 +1,17 @@
 const initialState = {
     categories: [
-        { category: 'electronics', name: "ELECTRONICS", description: 'Here you can find a lot of Electronic products' },
-        { category: 'mobile', name: "MOBILE PHONES", description: 'Here you can find a lot of MOBILE PHONES' },
-        { category: 'food', name: "FOOD", description: 'Here you can find a lot of food' },
+        { category: 'electrical', name: "ELECTRICAL", description: 'Here you can find a lot of Electronic products' },
+        { category: 'civil', name: "CIVIL", description: 'Here you can find a lot of MOBILE PHONES' },
+        { category: 'architect', name: "ARCHITECT", description: 'Here you can find a lot of food' },
+        { category: 'mechanic', name: "MECHANIC", description: 'Here you can find a lot of food' },
     ],
     activeCategory: {},
 }
-
 export default function reducer(state = initialState, action) {
-
     const { payload, type } = action;
-
     switch (type) {
         case 'ChangeActive':
             let modified = {};
-
             state.categories.forEach(item => {
                 if (item.category === payload) {
                     modified = item;
