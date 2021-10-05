0dfb329252722f761bd8551542e06b60d333973c
Yazan Baker <yazanbaker94@hotmail.com>
2021-10-05 21:23:15 +0300

    rdploy

--- src/components/OneProduct.js
+++ src/components/OneProduct.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
