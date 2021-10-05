2cdf184915445437008088f906a8ce7200e30226
Yazan Baker <yazanbaker94@hotmail.com>
2021-10-05 16:05:00 +0300

    deploy

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -73,12 +73,11 @@ function LoginForm() {
                           LOGIN
                         </button>
                         <Button  style={{ margin: '0 0 0 190px',color:'whitesmoke',width:'90px',height:'30px',border:'2px solid #15616D',borderRadius:'10px',backgroundColor:'#0f3b57',fontFamily:'Noto Sans Display',fontWeight:'bold',position:'relative',top:'-11px',right:'40px',fontSize:'15px'}} onClick={signup}> 
-                          <a href="/signup" >
-                            <Nav.Link href="/signup" style={{ margin: '0 0 0 190px',color:'whitesmoke',width:'90px',height:'30px',border:'2px solid #15616D',borderRadius:'10px',backgroundColor:'#0f3b57',fontFamily:'Noto Sans Display',fontWeight:'bold',position:'relative',top:'-7px',right:'203px',fontSize:'15px'}}>
+                          
                                 <Link to="/signup"  style={{display:'block',color:'whitesmoke',width:'100px',position:'relative',left:'-25px',top:'-5px'}}>SIGN UP
                                 </Link>
-                            </Nav.Link>
-                         </a>
+                            
+                        
                         </Button>
                         {/* <button
                           className="signup-btn"
