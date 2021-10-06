f8118c47a06ae8624332b0194eb213d4c3b3270a
razan-am <razanalamleh@gmail.com>
2021-10-06 11:56:47 +0300

    editing

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,8 @@ import LoginForm from "./auth/login";
 import './home.css';
 import logo from './logo.png';
 import homep from './img/homep.png';
+import { Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function Home() {
     return (
@@ -11,9 +13,15 @@ function Home() {
                 <img src={logo} alt="logo" className='logo1'/>
                 <h1>CUBE</h1>
                 <h3>A site for engineers, through which we can exchange experiences in various fields and purchase supplies that are used in various fields</h3>
-                {/* <img src={d} alt='background' className='image'/> */}
                 <img src={homep} alt='background' className='image1'/>
                 <LoginForm/>
+                <div className="homeF">
+                    <h2 className='homeh2'>CUBE STORE</h2>
+                    <p className="name1"> &copy; Copyright 2021 : CUBE STORE </p>
+
+                        <Link to="/AboutUs" style={{color:'#0f3b57',border:'1px solid #0f3b57',width:'110px',height:'30px',display:'block',position:'relative',top:'-90px',left:'1100px',}}>AboutUs
+                        </Link>
+                </div>
             </div>
         </div>
     )
